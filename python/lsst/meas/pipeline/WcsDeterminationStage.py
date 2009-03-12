3fd7ab138c58115efd0e0c38218acfdd9e5bb36b
rowen <rowen@git.lsstcorp.org>
2009-03-12 23:14:04 +0000

    Re-aimed at the moving target

--- python/lsst/meas/pipeline/WcsDeterminationStage.py
+++ python/lsst/meas/pipeline/WcsDeterminationStage.py
@@ -1,8 +1,8 @@
 from lsst.pex.harness.Stage import Stage
 
-# import lsst.pex.harness.Utils
 from lsst.pex.logging import Log
 
+# import lsst.pex.harness.Utils
 import lsst.daf.base as dafBase
 import lsst.afw.detection as afwDet
 import lsst.afw.image as afwImgage
@@ -11,16 +11,16 @@ import lsst.meas.astrom.net as astromNet
 class WcsDeterminationStage(Stage):
     """Refine a WCS in an Exposure based on a list of sources
 
-    See detection/pipeline/WcsDeterminationStageDictionary.paf
-    
-    DO WE NEED A CUT ON FLUX LIMIT? OR IS DETECTION DOING THAT FOR US?
+    See detection/pipeline/WcsDeterminationStageDictionary.paf for Policy file entries
 
     Clipboard Input:
-    - An Exposure whose name is given in the policy file
-    - A SourceSet whose name is given in the policy file
+    - a list of Exposures
+    - a SourceSet
+    - an initial Wcs
+    - various tuning parameters
 
     ClipboardOutput:
-    - The same Exposure, with refined Wcs
+    - The input Exposures with updated Wcs
     
     \todo Raise an exception instead of logging an error if the Wcs cannot be found
     (once the Wcs determination code is known to be reliable
@@ -33,46 +33,53 @@ class WcsDeterminationStage(Stage):
         # call base constructor
         lsst.pex.harness.Stage.Stage.__init__(self, stageId, policy)
         # initialize a log
-        self.log = Log(Log.getDefaultLog(), 
-                "lsst.meas.pipeline.WcsDeterminationStage")
+        self.log = Log(Log.getDefaultLog(), "lsst.meas.pipeline.WcsDeterminationStage")
         self.astromSolver = None
     
     def process(self):
         """Determine Wcs"""
         self.log.log(Log.INFO, "Wcs Determination Stage")
+
+        clipboard = self.inputQueue.getNextDataset()
         exposureNameList = self._policy.getStringArray("exposureNames")
-        sourceSetNameList = self._policy.getStringArray("sourceSetNames")
+        sourceSetName = self._policy.getString("sourceSetName")
+        initialWcsName = self._policy.getString("initialWcsName")
         self.fluxLimit = self._policy.getDouble("fluxLimit")
-        if len(exposureNameList) != len(sourceSetNameList):
+        if len(exposureNameList) != len(sourceSetName):
             raise RuntimeError("Numer of exposureNames = %s != number of sourceSetNames %s" %
                 (len(exposureNames), len(sourceSetNames)))
         
-        # read in astrometry.net indices, if not already done
+        # Read in astrometry.net indices, if not already done
         # todo: only read in required portions or mmap the files
         if self.astromSolver == None:
             self.astromSolver = astromNet.GlobalAstrometrySolution()
             #Read in the indices (i.e the files containing the positions of known asterisms
             #and add them to the astromSolver object
             astrometryIndicesGlob = self._policy.getString("astrometryIndicesGlob")
-            self.log.log(Log.INFO, "Reading astrometry index files %s" % (astrometryIndicesGlob,))
             indexPathList = glob.glob(astrometryIndicesGlob)
             for indexPath in indexPathList:
+                self.log.log(Log.INFO, "Reading astrometry index file %s" % (indexPath,))
                 self.astromSolver.addIndexFile(indexPath)
 
-        # set parameters
+        # Set parameters and compute Wcs
         allowDistortion = self._policy.getBoolean("allowDistortion")
         matchThreshold = self._policy.getDouble("matchThreshold")
         self.astromSolver.allowDistortion(allowDistortion)
         self.astromSolver.setMatchThreshold(matchThreshold)
+        sourceSet = clipboard.get(sourceSetName)
+        initialWcs = clipboard.get(initialWcsName)
+        self.log.log(Log.INFO, "Determine Wcs")
+        wcs = self.determineWcs(sourceSet, initialWcs)
 
-        for exposureName, sourceSetName in zip(exposureNameList, sourceSetNameList):
-            self.log.log(Log.INFO, "Determine Wcs of exposure %s using sourceSet %s" % (exposureName, sourceSetName))
+        # Update exposures
+        for exposureName in exposureNameList:
             exposure = clipboard.get(exposureName)
-            sourceSet = clipboard.get(sourceSetName)
-            self.processOneExposure(exposure, sourceSet)
+            exposure.setWcs(wcs)
 
-    def processOneExposure(self, exposure, sourceSet):
-        """Determine Wcs of an Exposure given a SourceSet
+        self.outputQueue.addDataSet(clipboard)
+    
+    def determineWcs(self, sourceSet, initialWcs):
+        """Determine Wcs of an Exposure given a SourceSet and an initial Wcs
         """
         # select sufficiently bright sources
         wcsSourceSet = afwDetection.SourceSet()
@@ -83,14 +90,19 @@ class WcsDeterminationStage(Stage):
         self.astromSolver.setStarlist(wcsSourceSet)
         self.astromSolver.setNumberStars(len(wcsSourceSet))
 
-        wcs = exposure.getWcs()
-        approxPixelScale = 3600.0 * math.sqrt(wcs.pixArea(wcs.getOriginRaDec()))
+        approxPixelScale = 3600.0 * math.sqrt(initialWcs.pixArea(initialWcs.getOriginRaDec()))
         pixelScaleRangeFactor = self._policy.getDouble("pixelScaleRangeFactor")
         self.astromSolver.setMinimumImageScale(approxPixelScale / pixelScaleRangeFactor)
         self.astromSolver.setMaximumImageScale(approxPixelScale * pixelScaleRangeFactor)
+        
+        # Set parity; once we know it, set it to save time
+        # values are: 0: not flipped, 1: flipped, 2: unknown
+        self.astromSolver.setParity(2)
 
+        wcs = afwImage.Wcs()
         if self.astromSolver.blindSolve():
             exposure.setWcs(wcs)
         else:
             # \todo: raise an exception once the Wcs determining code is known to be reliable
             self.log.log(Log.ERROR, "Failed to find WCS solution; leaving raw Wcs unchanged")
+        return wcs
