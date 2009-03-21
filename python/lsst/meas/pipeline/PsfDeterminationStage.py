c490751b8571d1ba5d83483271759923e2bd027e
rhl <rhl@git.lsstcorp.org>
2009-03-21 02:33:32 +0000

    Moved almost all the implementation into lsst.meas.algorithms.Psf

--- python/lsst/meas/pipeline/PsfDeterminationStage.py
+++ python/lsst/meas/pipeline/PsfDeterminationStage.py
@@ -53,135 +53,9 @@ class PsfDeterminationStage(Stage):
         self.outputQueue.addDataset(clipboard)
 
     def _impl(self, exposure, sourceList, sdqaRatings):
-        #
-        # Create an Image of Ixx v. Iyy, i.e. a 2-D histogram
-        #
-        psfHist = Psf.PsfShapeHistogram()
 
-        for source in sourceList:
-            if self._goodPsfCandidate(source):
-                psfHist.insert(source)
-        
-        clump = psfHist.getClump()
-        psfClumpX, psfClumpY, psfClumpIxx, psfClumpIxy, psfClumpIyy = clump
-        
-        #
-        # Go through and find all the PSF-like objects
-        #
-        mi = exposure.getMaskedImage()
-        #
-        # We'll split the image into a number of cells, 
-        # each of which contributes only one PSF candidate star
-        #
-        bbox = afwImg.BBox(mi.getXY0(), mi.getWidth(), mi.getHeight())
-        psfCellSet = afwMath.SpatialCellSet(bbox, 
-                                            self._sizeCellX, 
-                                            self._sizeCellY)
-        
-        psfStars = []
-        
-        det = psfClumpIxx*psfClumpIyy - psfClumpIxy*psfClumpIxy
-        try:
-            a, b, c = psfClumpIyy/det, -psfClumpIxy/det, psfClumpIxx/det
-        except ZeroDivisionError:
-            a, b, c = 1e4, 0, 1e4
-        
-        for source in sourceList:
-            Ixx, Ixy, Iyy = source.getIxx(), source.getIxy(), source.getIyy()
-            dx, dy = (Ixx - psfClumpX), (Iyy - psfClumpY)
-            
-            # A test for > would be confused by NaN's
-            if math.sqrt(a*dx*dx + 2*b*dx*dy + c*dy*dy) < 2: 
-                if not self._goodPsfCandidate(source):
-                    continue
-            try:
-                psfCandidate = algorithms.makePsfCandidate(source, mi)
-                psfCellSet.insertCandidate(psfCandidate)
-            except Exception, e:
-                continue
-            
-            psfStars += [source]
-        
-        #
-        # setWidth/setHeight are class static, 
-        # but we'd need to know that the class was <float> to use that info; 
-        # e.g.
-        #   afwMath.SpatialCellImageCandidateF_setWidth(21)
-        #
-        psfCandidate = algorithms.makePsfCandidate(source, mi)
-        psfCandidate.setWidth(21)
-        psfCandidate.setHeight(21)
-        del psfCandidate
-        
-        #
-        # Do a PCA decomposition of those PSF candidates
-        #
-        for iter in range(self._nIterForPsf):
-            for cell in psfCellSet.getCellList():
-                cell.setIgnoreBad(True)
-            
-            pair = algorithms.createKernelFromPsfCandidates(psfCellSet, 
-                                                self._nEigenComponents, 
-                                                self._spatialOrder,
-                                                self._kernelSize, 
-                                                self._nStarPerCell)
-            kernel, eigenValues = pair[0], pair[1]; 
-            del pair
-            
-            pair = algorithms.fitSpatialKernelFromPsfCandidates(kernel, 
-                                                psfCellSet, 
-                                                self._nStarPerCellSpatialFit,
-                                                self._tolerance)
-            status, chi2 = pair[0], pair[1]; 
-            del pair
-            
-            psf = algorithms.createPSF("PCA", kernel)
-            psfCandidate = algorithms.makePsfCandidate(source, mi)
-            
-            # number of degrees of freedom/star for chi2
-            nu = psfCandidate.getWidth()*psfCandidate.getHeight() - 1 
-            del psfCandidate
-            
-            for cell in psfCellSet.getCellList():
-                cell.setIgnoreBad(True)
-                for cand in cell:
-                    cand = algorithms.cast_PsfCandidateF(cand)
-                    rchi2 = cand.getChi2()/nu
-                    
-                    if rchi2 > self._reducedChi2ForPsfCandidates:
-                        cand.setStatus(afwMath.SpatialCellCandidate.BAD)
-                
-                cell.setIgnoreBad(True)
-        #
-        # Generate some stuff for SDQA
-        #
-        # Count PSF stars
-        #
-        numGoodStars = 0
-        numAvailStars = 0
-        for cell in psfCellSet.getCellList():
-            numGoodStars += cell.size()
-
-        for cell in psfCellSet.getCellList():
-            for cand in cell.begin(False):  # don't ignore BAD stars
-                numAvailStars += 1
-
-        sdqaRatings.append(sdqa.SdqaRating("phot.psf.spatialChi2", chi2,  -1, sdqa.SdqaRating.CCD))
-        sdqaRatings.append(sdqa.SdqaRating("phot.psf.numGoodStars", numGoodStars,  0, sdqa.SdqaRating.CCD))
-        sdqaRatings.append(sdqa.SdqaRating("phot.psf.numAvailStars", numAvailStars,  0, sdqa.SdqaRating.CCD))
-        sdqaRatings.append(sdqa.SdqaRating("phot.psf.spatialLowOrdFlag", 0,  0, sdqa.SdqaRating.CCD))
-        
-        return (psf, psfCellSet) 
+        return Psf.getPsf(exposure, sourceList, self._policy, sdqaRatings)
   
-    def _goodPsfCandidate(self, source):
-        """Should this object be included in the Ixx v. Iyy image?"""
-        if source.getFlagForDetection() & self._badFlags or \
-           source.getPsfFlux() < self._fluxLim: 
-            # ignore flagged or faint objects
-            return False
-
-        return True
-
     def _getClipboardData(self, clipboard):
         dataList = []
         dataPolicyList = self._policy.getPolicyArray("data")
@@ -201,10 +75,9 @@ class PsfDeterminationStage(Stage):
         return dataList
 
     def _validatePolicy(self):
-        if self._policy.exists("fluxLim"):
-            self._fluxLim = self._policy.get("fluxLim")
-        else:
-            self._fluxLim = 1000
+        # N.b. these aren't used in self, but the validation can't hurt --- until we have a dictionary
+        
+        self._fluxLim = self._policy.get("fluxLim")
         
         self._nEigenComponents = self._policy.getInt("nEigenComponents")
         self._spatialOrder  = self._policy.getInt("spatialOrder")
