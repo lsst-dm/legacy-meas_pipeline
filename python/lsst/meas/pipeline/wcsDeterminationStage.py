6c3dea2dcf617cfb87e3b9c380b4bdff4ae3db53
fergal <fergal@git.lsstcorp.org>
2009-12-16 20:39:11 +0000

    New wcs stage

--- python/lsst/meas/pipeline/wcsDeterminationStage.py
+++ python/lsst/meas/pipeline/wcsDeterminationStage.py
@@ -1,173 +1,227 @@
-import math
-import sys
-import lsst.utils as utils
+#! /usr/bin/env python
+
 import os
+import sys
+import math
+
+import eups
+import lsst.pex.harness as pexHarness
 import lsst.pex.harness.stage as harnessStage
-from lsst.pex.logging import Log
+from lsst.pex.harness.simpleStageTester import SimpleStageTester
 import lsst.pex.policy as pexPolicy
-import lsst.daf.base as dafBase
-import lsst.afw.detection as afwDet
-import lsst.afw.image as afwImage
+from lsst.pex.logging import Log, Debug, LogRec, Prop
+from lsst.pex.exceptions import LsstCppException
+import lsst.afw.image as afwImg
+
 import lsst.meas.astrom.net as astromNet
-import lsst.pex.exceptions as exceptions
-import lsst.meas.algorithms as measAlg
+import lsst.meas.astrom.sip as sip
+import lsst.meas.astrom.sip.cleanBadPoints as cleanBadPoints
+
+import pdb
+    
 
 class WcsDeterminationStageParallel(harnessStage.ParallelProcessing):
-    """Refine a WCS in an Exposure based on a list of sources
-
-    See detection/pipeline/WcsDeterminationStageDictionary.paf for Policy file entries
-
-    Clipboard Input:
-    - a list of Exposures
-    - a SourceSet
-    - an initial Wcs
-    - various tuning parameters
-
-    ClipboardOutput:
-    - The input Exposures with updated Wcs
+    """Validate the Wcs for an image using the astrometry.net package and calculate distortion
+    coefficients
     
-    \todo Raise an exception instead of logging an error if the Wcs cannot be found
-    (once the Wcs determination code is known to be reliable
-    \todo Found out if the astrometry.net indices are memory-mapped, and if not,
-    figure out how to only the required indices.
-    \todo After DC3 we should not assume the input exposure has a Wcs;
-    the associated minimal data (center RA/Dec, etc.) should come in as separate metadata.
+    Given a initial Wcs, and a list of sources (with pixel positions for each) in an image,
+    pass these to the astrometry_net package to verify the result. Then calculate
+    the distortion in the image and add that to the Wcs as SIP polynomials
+    
+    Clipboard Input:
+    - an Exposure containing a Wcs
+    - a SourceSet
+    
+    Clipboard Output
+    - A wcs
+    - A vector of SourceMatch objects
     """
+    
     def setup(self):
-        self.log = Log(self.log, "WcsDeterminationStage - parallel")
+        #I don't have the default policy in the correct place yet
+        policyFile=pexPolicy.DefaultPolicyFile("meas_pipeline",   # package name
+                                  "WcsDeterminationStageDictionary.paf", # default. policy
+                                  "policy" # dir containing policies
+                                  )
+        defaultPolicy = pexPolicy.Policy.createPolicy(policyFile, policyFile.getRepositoryPath())
         
+        #The stage can be called with an optional local policy file, which overrides the defaults
         #merge defaults
         policyFile = pexPolicy.DefaultPolicyFile("meas_pipeline",
             "WcsDeterminationStageDictionary.paf", "policy")
         defPolicy = pexPolicy.Policy.createPolicy(policyFile, policyFile.getRepositoryPath(), True)
 
         if self.policy is None:
-            self.policy = pexPolicy.Policy()
-        self.policy.mergeDefaults(defPolicy)
-
-        #get an GlobalAstrometrySolution
-        path = os.path.join(utils.productDir("astrometry_net_data"),
-                "metadata.paf")
-        self.astromSolver = astromNet.GlobalAstrometrySolution(path)
+            self.policy = defaultPolicy
+        else:
+            self.policy.mergeDefaults(defaultPolicy)
+               
+        #Setup the log
+        self.log = Debug(self.log, "WcsDeterminationStageParallel")
+        self.log.setThreshold(Log.DEBUG)
+        self.log.log(Log.INFO, "Finished setup of WcsDeterminationStageParallel")
+        
 
     def process(self, clipboard):
-        """Determine Wcs"""
-        self.log.log(Log.INFO, "Reset solver")
-        self.astromSolver.reset()
-
-        exposureKeyList = self.policy.getStringArray("exposureKeyList")
-        
-        ampBBox = clipboard.get(self.policy.getString("ampBBoxKey"))
-        sourceSet = clipboard.get(self.policy.getString("sourceSetKey"))
-        if isinstance(sourceSet, afwDet.PersistableSourceVector):
-            sourceSet = sourceSet.getSources()
-        
-        initialWcs = clipboard.get(exposureKeyList[0]).getWcs().clone()
-        
-        # Shift WCS from amp coordinates to CCD coordinates
-        # Use first Exposure's WCS as the initial guess
-        initialWcs.shiftReferencePixel(ampBBox.getX0(), ampBBox.getY0())
-
-        self.log.log(Log.INFO, "Determine Wcs")
-        wcs = self.determineWcs(sourceSet, initialWcs, self.policy)
-        self.log.log(Log.INFO, wcs.getFitsMetadata().toString())
-        
-        #output the ccd WCS
-        clipboard.put(self.policy.getString("outputCcdWcsKey", wcs))
-
-        # Shift WCS from CCD coordinates to amp coordinates
-        wcs.shiftReferencePixel(-ampBBox.getX0(), -ampBBox.getY0())
-
-        # Update exposures
-        for exposureKey in exposureKeyList:
-            exposure = clipboard.get(exposureKey)
-            exposure.setWcs(wcs.clone())
-
-    def determineWcs(self, sourceSet, initialWcs, policy):
-        """Determine Wcs of an Exposure given a SourceSet and an initial Wcs
-        """
-        # select sufficiently bright sources that are not flagged
-        fluxLimit = self.policy.getDouble("fluxLimit")
-        pixelScaleRangeFactor = self.policy.getDouble("pixelScaleRangeFactor")
+        self.log.log(Log.INFO, "Determining Wcs")
+        wcs = determineWcs(self.log, self.policy, clipboard)
 
 
-        # Set parameters
-        allowDistortion = self.policy.getBool("allowDistortion")
-        matchThreshold = self.policy.getDouble("matchThreshold")
-        self.astromSolver.allowDistortion(allowDistortion)
-        self.astromSolver.setMatchThreshold(matchThreshold)
-
-        # Shouldn't this be on the clipboard instead? But in any case,
-        # we need it to determine best guess RA/Dec of center of CCD
-        ccdWidth = self.policy.getInt("ccdDimensions.width")
-        ccdHeight = self.policy.getInt("ccdDimensions.height")
-        
-        wcsSourceSet = afwDet.SourceSet()
-        for source in sourceSet:
-            if source.getPsfFlux() >= fluxLimit and \
-                source.getFlagForDetection() == measAlg.Flags.BINNED1:
-                wcsSourceSet.append(source)
-        
-        self.log.log(Log.INFO, "Using %s sources with flux > %s; initial list had %s sources" % \
-            (len(wcsSourceSet), fluxLimit, len(sourceSet)))
-        self.astromSolver.setStarlist(wcsSourceSet)
-
-        if self.policy.exists("brightestNStars"):
-            num = self.policy.getInt("brightestNStars")
-            self.log.log(Log.INFO,
-                    "Setting number of stars in solver to %i" %(num,))
-            if num > wcsSourceSet.size():
-                num = wcsSourceSet.size()
-                self.log.log(Log.INFO, "Reducing to actual number: %i" % (num,))
-            self.astromSolver.setNumBrightObjects(num)
-
-        # find RA/Dec of center of image (need not be exact)
-        ccdCtrPos = afwImage.PointD(
-            afwImage.indexToPosition(ccdWidth / 2),
-            afwImage.indexToPosition(ccdHeight / 2),
-        )
-        # Use RA/decl of WCS origin, not CCD center, as it seems to lead to
-        # faster solutions.  TODO: figure out what the right input is.
-        # predRaDecCtr = initialWcs.xyToRaDec(ccdCtrPos)
-        predRaDecCtr = initialWcs.getOriginRaDec()
-        self.log.log(Log.INFO, "RA/Dec at initial WCS origin = %s, %s deg" % \
-            (predRaDecCtr.getX(), predRaDecCtr.getY()))
-        
-        # Determinate predicted image scale in arcseconds/pixel
-        pixelAreaDegSq = initialWcs.pixArea(ccdCtrPos) # in degrees^2
-        imageScale = math.sqrt(pixelAreaDegSq) * 3600.0
-        self.log.log(Log.INFO, "Predicted image scale = %s arcsec/pixel" % (imageScale,))
-    
-        minImageScale = imageScale / pixelScaleRangeFactor
-        maxImageScale = imageScale * pixelScaleRangeFactor
-        self.astromSolver.setMinimumImageScale(minImageScale)
-        self.astromSolver.setMaximumImageScale(maxImageScale)
-        self.log.log(Log.INFO, "Set image scale min=%s; max=%s" % (minImageScale, maxImageScale))
-    
-        if True: # set True once you trust the Wcs isFlipped function or remove the conditional
-            if initialWcs.isFlipped():
-                self.log.log(Log.INFO, "Set flipped parity")
-                self.astromSolver.setParity(astromNet.FLIPPED_PARITY)
-            else:
-                self.log.log(Log.INFO, "Set normal parity")
-                self.astromSolver.setParity(astromNet.NORMAL_PARITY)
-        else:
-            self.log.log(Log.INFO, "Set unknown parity")
-            self.astromSolver.setParity(astromNet.UNKNOWN_PARITY)
-
-        try:
-            solved = self.astromSolver.solve(predRaDecCtr.getX(), predRaDecCtr.getY())
-        except exceptions.LsstCppException:
-            err= sys.exc_info()[1]
-            self.log.log(Log.WARN, err.message.what())
-            solved = False
-            
-        if solved:
-            self.log.log(Log.WARN, "Found new wcs")
-            return self.astromSolver.getDistortedWcs()
-        else:
-            self.log.log(Log.WARN, "Failed to find WCS solution; leaving raw Wcs unchanged")
-            return initialWcs
 
 class WcsDeterminationStage(harnessStage.Stage):
-    parallelClas = WcsDeterminationStageParallel
+    """A wrapper stage that supplies the names of the classes that do the work
+       Different classes are provided for serial and parallel processing
+    """
+    parallelClass = WcsDeterminationStageParallel
+
+
+
+
+def determineWcs(log, policy, clipboard):
+
+    log.log(Log.INFO, "In determineWcs")
+
+    #Check inputs
+    if clipboard is None:
+        raise RuntimeError("Clipboard is empty")
+    
+    expKey = policy.get('inputExposureKey')
+    if not clipboard.contains(expKey):
+        raise RuntimeError("No exposure on clipboard")
+    exp = clipboard.get(expKey)
+    
+    srcSetKey=policy.get('inputSourceSetKey')
+    if not clipboard.contains(srcSetKey):
+        raise RuntimeError("No wcsSourceSet on clipboard")
+    srcSet = clipboard.get(srcSetKey)
+
+    outputWcsKey = policy.get('outputWcsKey')
+    
+    #Extract an initial guess wcs if available    
+    wcsIn = exp.getWcs() #May be None
+    if wcsIn is None:
+        log.log(log.WARN, "No wcs found on exposure. Doing blind solve")
+    
+    #Setup solver
+    path=os.path.join(eups.productDir("astrometry_net_data"), "metadata.paf")
+    solver = astromNet.GlobalAstrometrySolution(path)
+    solver.allowDistortion(policy.get('allowDistortion'))
+    matchThreshold = policy.get('matchThreshold')
+    solver.setMatchThreshold(matchThreshold)
+
+    log.log(log.DEBUG, "Setting starlist")
+    solver.setStarlist(srcSet)
+    log.log(log.DEBUG, "Setting numBrightObj")
+    solver.setNumBrightObjects(policy.get('numBrightStars'))
+
+    #Do a blind solve if we're told to, or if we don't have an input wcs
+    doBlindSolve = policy.get('blindSolve') or (wcsIn is None)
+    
+    log.log(log.DEBUG, "Solving")
+    if doBlindSolve or True:
+        isSolved = solver.solve()
+    else:
+        isSolved = solver.solve(wcsIn)
+
+    log.log(log.DEBUG, "Finished Solve step")
+    if isSolved == False:
+        log.log(log.WARN, "No solution found, using input Wcs")
+        clipboard.put(outputWcsKey, wcsIn)
+        return
+    
+    #
+    #Do sip corrections
+    #
+    #First obtain the catalogue-listed positions of stars
+    log.log(log.DEBUG, "Determining match objects")
+    linearWcs = solver.getWcs()
+    imgSizeInArcsec = getImageSizeInArcsec(srcSet, linearWcs)
+    cat = solver.getCatalogue(2*imgSizeInArcsec) #Catalogue of nearby stars
+    
+    #Now generate a list of matching objects, and store on the clipboard for later use
+    distInArcsec = policy.get('distanceForCatalogueMatchinArcsec')
+    cleanParam = policy.get('cleaningParameter')
+
+    matchList = matchSrcAndCatalogue(cat=cat, img=srcSet, wcs=linearWcs, 
+        distInArcsec=distInArcsec, cleanParam=cleanParam)
+            
+    if len(matchList) == 0:
+        log.log(Log.WARN, "No matches found between input source and catalogue.")
+        log.log(Log.WARN, "Something in wrong. Defaulting to input wcs")
+        clipboard.put(outputWcsKey, wcsIn)
+        return
+        
+    log.log(Log.INFO, "%i objects out of %i match sources listed in catalogue" %(len(matchList), len(srcSet)))
+        
+    clipboard.put(policy.get('outputMatchListKey'), matchList)
+    
+    #Now create a wcs with SIP polynomials
+    maxScatter = policy.get('wcsToleranceInArcsec')
+    maxSipOrder= policy.get('maxSipOrder')
+    print maxScatter, maxSipOrder
+    sipObject = sip.CreateWcsWithSip(matchList, linearWcs, maxScatter, maxSipOrder)
+    clipboard.put(outputWcsKey, sipObject.getNewWcs())
+    
+    log.log(Log.INFO, "Using %i th order SIP polynomial. Scatter is %.g arcsec" \
+        %(sipObject.getOrder(), sipObject.getScatterInArcsec()))
+
+    log.log(log.DEBUG, "Finish process")
+    solver.reset()
+    
+    
+def getImageSizeInArcsec(srcSet, wcs):
+    """ Get the approximate size of the image in arcseconds
+    
+    Input: 
+    srcSet List of detected objects in the image (with pixel positions)
+    wcs    Wcs converts pixel positions to ra/dec
+    
+    """
+    xfunc = lambda x: x.getXAstrom()
+    yfunc = lambda x: x.getYAstrom()
+    
+    x = map(xfunc, srcSet)
+    y = map(yfunc, srcSet)
+    
+    minx = min(x)
+    maxx = max(x)
+    miny = min(y)
+    maxy = max(y)
+    
+    llc = wcs.xyToRaDec(minx, miny)
+    urc = wcs.xyToRaDec(maxx, maxy)
+    
+    deltaRa = urc[0]-llc[0]
+    deltaDec = urc[1] - llc[1]
+    
+    #Approximately right
+    dist = math.sqrt(deltaRa**2 + deltaDec**2)
+    return dist*3600  #arcsec
+
+
+def matchSrcAndCatalogue(cat=None, img=None, wcs=None, distInArcsec=1.0, cleanParam=3):
+    """Given an input catalogue, match a list of objects in an image, given
+    their x,y position and a wcs solution.
+    
+    Return: A list of x, y, dx and dy. Each element of the list is itself a list
+    """
+    
+    if cat is None:
+        raise RuntimeError("Catalogue list is not set")
+    if img is None:
+        raise RuntimeError("Image list is not set")
+    if wcs is None:
+        raise RuntimeError("wcs is not set")
+    
+        
+    matcher = sip.MatchSrcToCatalogue(cat, img, wcs, distInArcsec)    
+    matchList = matcher.getMatches()
+    
+
+    if matchList is None:
+        raise RuntimeError("No matches found between image and catalogue")
+
+    matchList = cleanBadPoints.clean(matchList, wcs, nsigma=cleanParam)
+    return matchList
+
+
